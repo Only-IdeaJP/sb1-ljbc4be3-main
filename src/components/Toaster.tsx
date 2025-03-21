6b776a6f8d95e014273fcfce796620163cdd7746
Kacper <105833652+Kelposs@users.noreply.github.com>
2025-03-22 07:46:28 +0900

    Update Toaster.tsx

--- src/components/Toaster.tsx
+++ src/components/Toaster.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Toaster, toast } from 'react-hot-toast';
 
@@ -45,24 +44,3 @@ export const ToastContainer: React.FC = () => {
         />
     );
 };
-=======
-import { Toaster, toast } from 'react-hot-toast';
-
-interface HotToastType {
-  success: (message: string) => void;
-  error: (message: string) => void;
-  info: (message: string) => void;
-}
-
-const HotToast: HotToastType = {
-  success: (message: string) => toast.success(message),
-  error: (message: string) => toast.error(message),
-  info: (message: string) => toast(message),
-};
-
-const ToastContainer: React.FC = () => {
-  return <Toaster position="top-right" toastOptions={{ duration: 3000 }} />;
-};
-
-export { HotToast, ToastContainer };
->>>>>>> main
